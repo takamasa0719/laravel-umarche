de69dc4900508b70bc801b23adeaf9c76857d40c
taka0719 <rabbitlover0719@ezweb.ne.jp>
2022-09-06 11:28:34 +0900

    shopCreateDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Owner;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Throwable;
+use Illuminate\Support\Facades\Log;
+use App\Models\Shop;
 
 class OwnersController extends Controller
 {
@@ -72,11 +75,28 @@ class OwnersController extends Controller
             'password' => 'required|string|confirmed|min:8',
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try{
+            DB::transaction(function() use($request) {
+                $owner = Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください。',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true,
+                ]);
+            }, 2);
+        }catch(Throwable $e){
+            Log::error($e);
+            throw($e);
+        }
+
+        
 
         return redirect()->route('admin.owners.index')->with([
             'message' => 'オーナー登録を実施しました。',
